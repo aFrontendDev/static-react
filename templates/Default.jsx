24940046d563e868a7ca445fcc31a9e4277f5e02
ablackledge <a.blackledge@building-blocks.com>
2017-06-22 21:12:18 +0100

    converting json

--- templates/Default.jsx
+++ templates/Default.jsx
@@ -4,21 +4,59 @@ import React from 'react';
 import withInitialProps from 'metalsmith-react-templates/withInitialProps';
 
 class Default extends React.Component {
-  state = {}
 
-  componentDidMount = () => {
-    this.setState({
-      contents: this.props.contents,
-      inputValue: this.props.inputValue
-    });
+  constructor(props) {
+    super(props);
+
+    const jsonData = JSON.parse(this.props.contents);
+    this.state = {
+      data: jsonData
+    };
   }
 
   render() {
+    let jsonData = this.state.data;
+    let tagsLength = jsonData.tags ? jsonData.tags.length : null;
+    let ctaLength = jsonData.ctas ? jsonData.ctas.length : null;
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.test}</p>
-        <p>{this.props.contents}</p>
+      <div id={jsonData.id}>
+        <h1>{jsonData.title}</h1>
+
+        {
+          tagsLength ?
+          <ul>
+          {
+            jsonData.tags.map(function (item, index) {
+              return <li key={index}>{item}</li>
+            })
+          }
+          </ul>
+          : null
+        }
+
+        {
+          ctaLength ?
+          <div class="ctas">
+            {
+              jsonData.ctas.map( (cta, index) => {
+                return (
+                  <p key={index}>
+                    <a href={cta.href} target="_blank">
+                    {cta.text}
+                    </a>
+                  </p>
+                );
+              })
+            }
+          </div>
+          : null
+        }
+
+        <picture>
+            <source media="(min-width: 1024px)" srcSet={jsonData.image.lrg} />
+            <source media="(min-width: 768px)" srcSet={jsonData.image.med} />
+            <img srcSet={jsonData.image.small} src={jsonData.image.lrg} alt="alt text" />
+        </picture>
       </div>
     )
   }
